b05e537fa24cdc8ccb039074d1478a0b26aef82c
Sukanayna Hoo <sukanayna.hoo@mymona.uwi.edu>
2025-11-29 23:06:45 -0500

    Completed exercise 4

--- world.php
+++ world.php
@@ -12,8 +12,20 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+<?php 
+echo "
+<table>
+  <tr>
+    <th>Country Name</th>
+    <th>Continent</th>
+    <th>Independence Year</th>
+    <th>Head of State</th>
+  </tr>";
+foreach ($results as $row):
+  echo "<tr><td>" . $row['name'] . "</td><td>". $row['continent'] . "</td><td>" . $row['independence_year'] 
+  . "</td><td>" . $row['head_of_state'] . "</td></tr>"; 
+ endforeach; 
+ echo "</table>";
+ ?>
+
+
